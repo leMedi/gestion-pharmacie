72ddd453c36f0b130b1583bc708b9e66d0987f41
Mehdi Elhaij <mehdi.elhaij@hotmail.com>
2017-11-17 11:40:05 +0000

    config File

---
+++ include/config.php
@@ -0,0 +1,9 @@
+<?php
+
+    ///////////////
+    /// Configuration du base de donner
+    /////////
+    define('db_host', 'localhost');
+    define('db_name', '');
+    define('db_user', '');
+    define('db_pass', '');
