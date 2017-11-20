9890a9e7d7100ec21113d0f9c5ab25c7805c4d31
Mehdi Elhaij <mehdi.elhaij@hotmail.com>
2017-11-20 15:59:52 +0000

    backoffice page

---
+++ backoffice.php
@@ -0,0 +1,28 @@
+<?php
+
+require __DIR__. '/include/outils.php';
+
+
+?>
+
+
+<?php
+    template('header');
+?>
+
+<div class="wrapper">
+    <div class="sidebar" data-color="blue" data-image="public/img/sidebar-5.jpg">
+        <?php template('sidebar'); ?> 
+    </div> <!-- .sidebar -->
+
+    <div class="main-panel">
+        <?php template('nav'); ?> 
+    
+        <div class="content">
+            <div class="container-fluid">
+                
+            </div>
+        </div> <!-- .content -->
+<?php
+    template('footer');
+?>
