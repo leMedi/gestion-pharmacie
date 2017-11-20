e928b38ae96a26543d56ae03f2d072e7d7f569e8
achraf elyaacoubi <achraf18el@gmail.com>
2017-11-20 17:30:44 +0000

    relative assets path

--- templates/footer.php
+++ templates/footer.php
@@ -1,15 +1,15 @@
     <!--   Core JS Files   -->
-    <script src="public/js/jquery.3.2.1.min.js" type="text/javascript"></script>
-	<script src="public/js/bootstrap.min.js" type="text/javascript"></script>
+    <script src="<?= isset($path) ? $path : '' ?>public/js/jquery.3.2.1.min.js" type="text/javascript"></script>
+	<script src="<?= isset($path) ? $path : '' ?>public/js/bootstrap.min.js" type="text/javascript"></script>
 
 	<!--  Charts Plugin -->
-	<script src="public/js/chartist.min.js"></script>
+	<script src="<?= isset($path) ? $path : '' ?>public/js/chartist.min.js"></script>
 
     <!--  Notifications Plugin    -->
-    <script src="public/js/bootstrap-notify.js"></script>
+    <script src="<?= isset($path) ? $path : '' ?>public/js/bootstrap-notify.js"></script>
 
     <!-- Light Bootstrap Table Core javascript and methods for Demo purpose -->
-    <script src="public/js/light-bootstrap-dashboard.js?v=1.4.0"></script>
+    <script src="<?= isset($path) ? $path : '' ?>public/js/light-bootstrap-dashboard.js?v=1.4.0"></script>
     
-	<script src="public/js/master.js"></script>
+	<script src="<?= isset($path) ? $path : '' ?>public/js/master.js"></script>
 </body>
--- templates/header.php
+++ templates/header.php
@@ -12,19 +12,19 @@
 
 
     <!-- Bootstrap core CSS     -->
-    <link href="public/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="<?= isset($path) ? $path : '' ?>public/css/bootstrap.min.css" rel="stylesheet" />
 
     <!-- Animation library for notifications   -->
-    <link href="public/css/animate.min.css" rel="stylesheet"/>
+    <link href="<?= isset($path) ? $path : '' ?>public/css/animate.min.css" rel="stylesheet"/>
 
     <!--  Light Bootstrap Table core CSS    -->
-    <link href="public/css/light-bootstrap-dashboard.css?v=1.4.0" rel="stylesheet"/>
+    <link href="<?= isset($path) ? $path : '' ?>public/css/light-bootstrap-dashboard.css?v=1.4.0" rel="stylesheet"/>
 
     <!--     Fonts and icons     -->
     <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
     <link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300' rel='stylesheet' type='text/css'>
-    <link href="public/css/pe-icon-7-stroke.css" rel="stylesheet" />
+    <link href="<?= isset($path) ? $path : '' ?>public/css/pe-icon-7-stroke.css" rel="stylesheet" />
 
-    <link href="public/css/master.css" rel="stylesheet" />
+    <link href="<?= isset($path) ? $path : '' ?>public/css/master.css" rel="stylesheet" />
 </head>
 <body>
