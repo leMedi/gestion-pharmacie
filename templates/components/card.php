f1c025e07903f6acba0bf2dd973e62d831e3e171
Mehdi Elhaij <mehdi.elhaij@hotmail.com>
2017-11-20 16:11:23 +0000

    card component

---
+++ templates/components/card.php
@@ -0,0 +1,21 @@
+<div class="card">
+    <div class="header">
+        <h4 class="title">Email Statistics</h4>
+        <p class="category">Last Campaign Performance</p>
+    </div>
+    <div class="content">
+        <div id="chartPreferences" class="ct-chart ct-perfect-fourth"></div>
+
+        <div class="footer">
+            <div class="legend">
+                <i class="fa fa-circle text-info"></i> Open
+                <i class="fa fa-circle text-danger"></i> Bounce
+                <i class="fa fa-circle text-warning"></i> Unsubscribe
+            </div>
+            <hr>
+            <div class="stats">
+                <i class="fa fa-clock-o"></i> Campaign sent 2 days ago
+            </div>
+        </div>
+    </div>
+</div>
