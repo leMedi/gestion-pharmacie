8064a89cb784709373c7f89159087b9bd1a3f5bd
Mehdi Elhaij <mehdi.elhaij@hotmail.com>
2017-11-20 15:58:05 +0000

    basic templates

---
+++ templates/sidebar.php
@@ -0,0 +1,22 @@
+<div class="sidebar-wrapper">
+    <div class="logo">
+        <a href="http://www.creative-tim.com" class="simple-text">
+            Creative Tim
+        </a>
+    </div>
+
+    <ul class="nav">
+        <li class="active">
+            <a href="dashboard.html">
+                <i class="pe-7s-graph"></i>
+                <p>Dashboard</p>
+            </a>
+        </li>
+        <li>
+            <a href="user.html">
+                <i class="pe-7s-user"></i>
+                <p>User Profile</p>
+            </a>
+        </li>
+    </ul>
+</div> <!-- .sidebar-wrapper -->
