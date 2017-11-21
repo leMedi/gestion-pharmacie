1faca0f0dbee31e2bccc0d9224e7776d6dee8eaa
Mehdi Elhaij <mehdi.elhaij@hotmail.com>
2017-11-21 18:19:26 +0000

    statistics card component

---
+++ templates/components/statscard.php
@@ -0,0 +1,12 @@
+<div class="card">
+    <div class="header">
+        <h4 class="title"><?= $title ?></h4>
+        <p class="category"><?= $subtitle ?></p>
+        
+    </div>
+    <div class="content">
+        <h1><?= $stats ?></h1>
+        <div class="footer">
+        </div>
+    </div>
+</div>
