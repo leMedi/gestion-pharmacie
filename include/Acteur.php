12ac4919a0fc305126377ff5d43f408e311810db
Mehdi Elhaij <mehdi.elhaij@hotmail.com>
2017-11-22 14:12:39 +0000

    medicament: model

--- include/Acteur.php
+++ include/Acteur.php
@@ -4,4 +4,27 @@ require_once('outils.php');
 
 
 
+/**
+* Model Medicaments
+*/
+class Medicaments extends basedonner
+{
+    protected $table = 'medicaments';
+    protected $columns = array('nom', 'ref', 'form', 'prix_achat',
+                             'prix_vente', 'qte', 'aille', 'col',
+                            'etage', 'expiration',  'description');
+
+    function __construct($donner = null)
+    {
+        Parent::__construct($this->table, $this->columns);
+
+        if($donner){
+            $this->remplire($donner);
+        }
+
+    }
+}
+
+
+
 
