036832de9623d03c0916bd844ab3fbd6c7889b0a
Mehdi Elhaij <mehdi.elhaij@hotmail.com>
2017-11-22 13:58:03 +0000

    medicament: schema du tableau

--- include/basedonner.sql
+++ include/basedonner.sql
@@ -12,3 +12,19 @@ CREATE TABLE employers(
    admin    BOOLEAN         NOT NULL default 0,
    PRIMARY KEY (id)
 );
+
+CREATE TABLE Medicaments(
+   id           INT             NOT NULL,
+   nom          VARCHAR (20)    NOT NULL,
+   ref          VARCHAR (5)     NOT NULL,
+   form         VARCHAR (15)    NOT NULL,
+   prix_achat   INT             NOT NULL,
+   prix_vente   FLOAT(7,2)      NOT NULL,
+   qte          INT             UNSIGNED NOT NULL,
+   aille        INT(3)          UNSIGNED,
+   col          INT(3)          UNSIGNED,
+   etage        INT(3)          UNSIGNED,
+   expiration   DATE            NOT NULL,
+   description  TEXT,
+   PRIMARY KEY (id)
+);
