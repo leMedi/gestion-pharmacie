d76e268b16471a5ea55ad67770024de683356dd5
Mehdi Elhaij <mehdi.elhaij@hotmail.com>
2017-11-24 00:58:10 +0000

    backoffice: afficher les statistiques

--- backoffice.php
+++ backoffice.php
@@ -6,6 +6,12 @@ pageProteger();
 
 ?>
 
+<?php
+    $totalMeds = $current_user->sql('SELECT COUNT(*) as total FROM medicaments;', array())->total;
+    $totalClients = $current_user->sql('SELECT COUNT(*) as total FROM clients;', array())->total;
+    $totalEmployes = $current_user->sql('SELECT COUNT(*) as total FROM employers;', array())->total;
+    $totalCredit = $current_user->sql('SELECT SUM(montant_restant) as total FROM clients;', array())->total;
+?>
 
 <?php
     template('header');
@@ -17,40 +23,353 @@ pageProteger();
     </div> <!-- .sidebar -->
 
     <div class="main-panel">
-        <?php template('nav'); ?> 
+        <?php template('nav'); ?>
     
         <div class="content">
             <div class="container-fluid">
                 <div class="row">
-                    <div class="col-md-3">
+                    <div class="col-md-2">
+                        <a href="<?= lien('/medicaments/') ?> ">
                         <?php template('components/statscard', array(
-                                    'title' => 'Medicament',
-                                    'subtitle' => 'Nombre total des medicament',
-                                    'stats' => '104',
+                                    'title' => 'Medicaments',
+                                    'subtitle' => 'Nombre total des medicaments',
+                                    'stats' => $totalMeds,
                                 )
                         ); ?>
+                        </a>
                     </div>
 
-                    <div class="col-md-3">
+                    <div class="col-md-2">
+                        <a href="<?= lien('/medicaments/critique.php') ?> ">
+                        <?php template('components/statscard', array(
+                                    'title' => 'Quantité Critique',
+                                    'subtitle' => 'medicaments avec qte < 10',
+                                    'stats' => $totalEmployes,
+                                )
+                        ); ?>
+                        </a>
+                    </div>
+
+                    <div class="col-md-2">
+                        <a href="<?= lien('/client/') ?> ">
                         <?php template('components/statscard', array(
                                     'title' => 'Clients Fidele',
                                     'subtitle' => 'Nombre total des Client',
-                                    'stats' => '57',
+                                    'stats' => $totalClients,
                                 )
                         ); ?>
+                        </a>
                     </div>
 
-                    <div class="col-md-3">
+                    <div class="col-md-2">
+                        <a href="<?= lien('/client/credit.php') ?> ">
                         <?php template('components/statscard', array(
-                                    'title' => 'Cachiers',
-                                    'subtitle' => 'Nombre total des cachiers',
-                                    'stats' => '5',
+                                    'title' => 'Credits',
+                                    'subtitle' => 'Somme total des Credits',
+                                    'stats' => $totalCredit . ' DH',
                                 )
                         ); ?>
+                        </a>
+                    </div>
+
+                    <div class="col-md-2">
+                        <a href="<?= lien('/employe/') ?> ">
+                            <?php template('components/statscard', array(
+                                        'title' => 'Employées',
+                                        'subtitle' => 'Nombre total des employées',
+                                        'stats' => $totalEmployes,
+                                    )
+                            ); ?>
+                        </a>
                     </div>
                 </div>
-            </div>
+
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="card">
+
+                            <div class="header">
+                                <h4 class="title">Statistics des Credits</h4>
+                                <p class="category">Graph des credits</p>
+                            </div>
+                            <div class="content">
+                                <div id="chartPreferences" class="ct-chart ct-perfect-fourth"></div>
+
+                                <div class="footer">
+                                    <div class="legend">
+                                        <i class="fa fa-circle text-info"></i> Payer
+                                        <i class="fa fa-circle text-danger"></i> Reste
+                                    </div>
+                                    <hr>
+                                    <div class="stats">
+                                        <i class="fa fa-clock-o"></i> le dernier mois
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-8">
+                        <div class="card">
+                            <div class="header">
+                                <h4 class="title">Statistics de vente</h4>
+                                <p class="category">Graph de vente</p>
+                            </div>
+                            <div class="content">
+                                <div id="chartActivity" class="ct-chart"></div>
+                                <div class="footer">
+                                    <div class="legend">
+                                        <i class="fa fa-circle text-info"></i> Medicaments
+                                        <i class="fa fa-circle text-danger"></i> Vente
+                                    </div>
+                                    <hr>
+                                    <div class="stats">
+                                        <i class="fa fa-history"></i> Le dernier mois
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+            </div> <!-- container -->
         </div> <!-- .content -->
 <?php
     template('footer');
 ?>
+
+<script>
+    type = ['','info','success','warning','danger'];
+
+
+demo = {
+    initPickColor: function(){
+        $('.pick-class-label').click(function(){
+            var new_class = $(this).attr('new-class');
+            var old_class = $('#display-buttons').attr('data-class');
+            var display_div = $('#display-buttons');
+            if(display_div.length) {
+            var display_buttons = display_div.find('.btn');
+            display_buttons.removeClass(old_class);
+            display_buttons.addClass(new_class);
+            display_div.attr('data-class', new_class);
+            }
+        });
+    },
+
+    checkScrollForTransparentNavbar: debounce(function() {
+            $navbar = $('.navbar[color-on-scroll]');
+            scroll_distance = $navbar.attr('color-on-scroll') || 500;
+
+            if($(document).scrollTop() > scroll_distance ) {
+                if(transparent) {
+                    transparent = false;
+                    $('.navbar[color-on-scroll]').removeClass('navbar-transparent');
+                    $('.navbar[color-on-scroll]').addClass('navbar-default');
+                }
+            } else {
+                if( !transparent ) {
+                    transparent = true;
+                    $('.navbar[color-on-scroll]').addClass('navbar-transparent');
+                    $('.navbar[color-on-scroll]').removeClass('navbar-default');
+                }
+            }
+    }, 17),
+
+    initDocChartist: function(){
+        var dataSales = {
+          labels: ['9:00AM', '12:00AM', '3:00PM', '6:00PM', '9:00PM', '12:00PM', '3:00AM', '6:00AM'],
+          series: [
+             [287, 385, 490, 492, 554, 586, 698, 695, 752, 788, 846, 944],
+            [67, 152, 143, 240, 287, 335, 435, 437, 539, 542, 544, 647],
+            [23, 113, 67, 108, 190, 239, 307, 308, 439, 410, 410, 509]
+          ]
+        };
+
+        var optionsSales = {
+          lineSmooth: false,
+          low: 0,
+          high: 800,
+          showArea: true,
+          height: "245px",
+          axisX: {
+            showGrid: false,
+          },
+          lineSmooth: Chartist.Interpolation.simple({
+            divisor: 3
+          }),
+          showLine: false,
+          showPoint: false,
+        };
+
+        var responsiveSales = [
+          ['screen and (max-width: 640px)', {
+            axisX: {
+              labelInterpolationFnc: function (value) {
+                return value[0];
+              }
+            }
+          }]
+        ];
+
+        Chartist.Line('#chartHours', dataSales, optionsSales, responsiveSales);
+
+
+        var data = {
+          labels: ['Jan', 'Feb', 'Mar', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+          series: [
+            [542, 443, 320, 780, 553, 453, 326, 434, 568, 610, 756, 895],
+            [412, 243, 280, 580, 453, 353, 300, 364, 368, 410, 636, 695]
+          ]
+        };
+
+        var options = {
+            seriesBarDistance: 10,
+            axisX: {
+                showGrid: false
+            },
+            height: "245px"
+        };
+
+        var responsiveOptions = [
+          ['screen and (max-width: 640px)', {
+            seriesBarDistance: 5,
+            axisX: {
+              labelInterpolationFnc: function (value) {
+                return value[0];
+              }
+            }
+          }]
+        ];
+
+        Chartist.Bar('#chartActivity', data, options, responsiveOptions);
+
+        var dataPreferences = {
+            series: [
+                [25, 30]
+            ]
+        };
+
+        var optionsPreferences = {
+            donut: true,
+            donutWidth: 40,
+            startAngle: 0,
+            total: 100,
+            showLabel: false,
+            axisX: {
+                showGrid: false
+            }
+        };
+
+        // Chartist.Pie('#chartPreferences', dataPreferences, optionsPreferences);
+
+        Chartist.Pie('#chartPreferences', {
+          labels: ['62%','32%'],
+          series: [62, 32]
+        });
+    },
+
+    initChartist: function(){
+
+        var dataSales = {
+          labels: ['9:00AM', '12:00AM', '3:00PM', '6:00PM', '9:00PM', '12:00PM', '3:00AM', '6:00AM'],
+          series: [
+             [287, 385, 490, 492, 554, 586, 698, 695, 752, 788, 846, 944],
+            [67, 152, 143, 240, 287, 335, 435, 437, 539, 542, 544, 647],
+            [23, 113, 67, 108, 190, 239, 307, 308, 439, 410, 410, 509]
+          ]
+        };
+
+        var optionsSales = {
+          lineSmooth: false,
+          low: 0,
+          high: 800,
+          showArea: true,
+          height: "245px",
+          axisX: {
+            showGrid: false,
+          },
+          lineSmooth: Chartist.Interpolation.simple({
+            divisor: 3
+          }),
+          showLine: false,
+          showPoint: false,
+        };
+
+        var responsiveSales = [
+          ['screen and (max-width: 640px)', {
+            axisX: {
+              labelInterpolationFnc: function (value) {
+                return value[0];
+              }
+            }
+          }]
+        ];
+
+        Chartist.Line('#chartHours', dataSales, optionsSales, responsiveSales);
+
+
+        var data = {
+          labels: ['Jan', 'Feb', 'Mar', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+          series: [
+            [542, 443, 320, 780, 553, 453, 326, 434, 568, 610, 756, 895],
+            [412, 243, 280, 580, 453, 353, 300, 364, 368, 410, 636, 695]
+          ]
+        };
+
+        var options = {
+            seriesBarDistance: 10,
+            axisX: {
+                showGrid: false
+            },
+            height: "245px"
+        };
+
+        var responsiveOptions = [
+          ['screen and (max-width: 640px)', {
+            seriesBarDistance: 5,
+            axisX: {
+              labelInterpolationFnc: function (value) {
+                return value[0];
+              }
+            }
+          }]
+        ];
+
+        Chartist.Bar('#chartActivity', data, options, responsiveOptions);
+
+        var dataPreferences = {
+            series: [
+                [25, 30, 20, 25]
+            ]
+        };
+
+        var optionsPreferences = {
+            donut: true,
+            donutWidth: 40,
+            startAngle: 0,
+            total: 100,
+            showLabel: false,
+            axisX: {
+                showGrid: false
+            }
+        };
+
+        // Chartist.Pie('#chartPreferences', dataPreferences, optionsPreferences);
+
+        Chartist.Pie('#chartPreferences', {
+          labels: ['62%','32%'],
+          series: [62, 32]
+        });
+    },
+
+
+
+
+
+}
+
+demo.initChartist();
+
+</script>
